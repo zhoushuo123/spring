b3f72e710035cf5d6be49a6fc44e514bb8530640
Shuo Zhou <kingzhoushuo@outlook.com>
2021-10-11 22:05:44 +0800

    测试提交2

--- stu/zs/java/basics/StringmethodsTest.java
+++ stu/zs/java/basics/StringmethodsTest.java
@@ -2,6 +2,8 @@ package zs.java.basics;
 
 public class StringmethodsTest {
     public static void main(String[] args) {
-        String greeting = "";
+        String greeting = "hello";
+        greeting="help!";
+        System.out.println(greeting);
     }
 }
